e9baa768257e73884040bc601c65379d08c3711b
Madhankumar13082006 <madhankumarthangaraj13@gmail.com>
2026-01-22 09:56:29 +0530

    kernal.asm - commands add for better understanding

--- kernal.asm
+++ kernal.asm
@@ -1,11 +1,11 @@
-org 0x0500
-bits 16
+org 0x0500  ; starting of sector or location
+bits 16 ; 16 bit in real mode
 
 start:
-    mov ah, 0x0E
-    mov al, 'M'
-    int 0x10
+    mov ah, 0x0E ;acculator high just select character print sunction like 
+    mov al, 'M' ; then print it print M
+    int 0x10 ;which location to print like grid of the computer screen 
 
-hang:
+hang: ; safe stop
     hlt
     jmp hang
